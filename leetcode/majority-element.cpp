179c0bb0601dba38200b83076f214d7071614156
Ibtid <ibtid011@example.com>
2022-09-15 00:24:31 +0600

    majority elements

---
+++ leetcode/majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i <= nums.size() / 2; i++)
+        {
+            if (nums[i] == nums[i + nums.size() / 2])
+            {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+};
